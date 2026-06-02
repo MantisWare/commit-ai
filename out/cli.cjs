9ba5dfa90b3aef2badb1c4ccb2abd40e5eaff43f
Waldo Marais <waldo.marais@domo.com>
2026-06-02 08:30:56 -0500

    build

--- out/cli.cjs
+++ out/cli.cjs
@@ -46521,7 +46521,7 @@ function G3(t2, e3) {
 // package.json
 var package_default = {
   name: "@mantisware/commit-ai",
-  version: "1.0.11",
+  version: "1.0.12",
   description: "Create amazing commits in just seconds. Say farewell to boring commits with AI! \u{1F92F}\u{1F525}",
   keywords: [
     "git",
